76d009af55fdc817c0bc70bb7290ca77d4486c60
gaukharka <gaukhar.kashaganova@gmail.com>
2020-09-13 10:31:34 +0200

    Updates

---
+++ precache-manifest.69bb928c4004a6e26a2559441ef4318e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ff7812c73f603e5ca979885e35207f9",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "a2707d70ef56b795439c",
+    "url": "/todoapp/static/js/2.d0581805.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoapp/static/js/2.d0581805.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e621afc1bf44acf40fc",
+    "url": "/todoapp/static/js/main.c637880f.chunk.js"
+  },
+  {
+    "revision": "80b05f8f46ecf0bf2c2b",
+    "url": "/todoapp/static/js/runtime-main.5545d9fc.js"
+  }
+]);
